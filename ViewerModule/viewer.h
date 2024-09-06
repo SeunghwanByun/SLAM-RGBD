bd2d6329f6a7647df2e8b4035d772d789d8027b8
shbyun <rapping44@naver.com>
2024-09-07 00:22:12 +0900

    modify viewer module

--- ViewerModule/viewer.h
+++ ViewerModule/viewer.h
@@ -1,6 +1,7 @@
 #ifndef VIEWER_H
 #define VIEWER_H
-#include "../SensorModule/astra_wrapper.h"  // 헤더 파일 포함
+// #include "../SensorModule/astra_wrapper.h"  // 헤더 파일 포함
+#include "../main.h"
 
 #include <GL/glew.h>
 #include <GL/glut.h>
