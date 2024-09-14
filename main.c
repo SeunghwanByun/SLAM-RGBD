d4f21a2475adfcb2fc8be0993bce50bd4a3d1565
shbyun <rapping44@naver.com>
2024-09-14 23:07:16 +0900

    comment

--- main.c
+++ main.c
@@ -11,12 +11,14 @@ int main(int argc, char** argv)
     pthread_t sensor_thread_id;
     pthread_create(&sensor_thread_id, NULL, sensorModule, NULL);
 
+    /* Generate 3D Viewer. */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
     pthread_t viewer_thread_id;
     pthread_create(&viewer_thread_id, NULL, viewerModule, NULL);
 
+    /* Logging system will be here. */
     pthread_t logging_thread_id;
     pthread_create(&logging_thread_id, NULL, loggingModule, NULL);
 
