cf73a1a45204b796a6e4552b22c53079860211a9
Seunghwan Byun <49049277+SeunghwanByun@users.noreply.github.com>
2024-10-02 23:41:40 +0900

    make algorithm module (#24)

--- main.h
+++ main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 #include "LoggingModule/loggingModule.h"
-#include "AlgorithmModule/Slam.h"
 #include "SensorModule/sensorModule.h"
 #include "ViewerModule/viewerModule.h"
+#include "AlgorithmModule/algorithmModule.h"
 
 #endif
